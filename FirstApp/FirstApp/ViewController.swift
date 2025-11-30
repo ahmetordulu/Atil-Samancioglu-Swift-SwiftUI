66a7b4e8326c30892592066ce8b659cd75584b67
Ahmet Rıdvan Ordulu <ahmetridvanordulu@icloud.com>
2025-12-01 00:53:13 +0300

    Initial commit for FirstApp iOS project
    Add initial Xcode project files, including AppDelegate, SceneDelegate, ViewController, asset catalogs, storyboards, and configuration files for the FirstApp iOS application.

---
+++ FirstApp/FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Ahmet Rıdvan Ordulu on 11/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+  
+
+}
+
